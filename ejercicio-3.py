ce26e36cb72d54f7ed8dea5d8c37540855387027
JuanEstebanFlorezCarmona <jesteban-florez@hotmail.com>
2025-04-21 21:58:01 -0500

    Add files via upload

---
+++ ejercicio-3.py
@@ -0,0 +1,7 @@
+numero =int (input("ingrese un numero: "))
+if numero == 0:
+    print("el numero es cero")
+elif numero % 2 ==0 :
+    print("El numero es par")
+else:
+    print ("el numero es impar")
