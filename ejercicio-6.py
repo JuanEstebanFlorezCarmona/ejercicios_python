9d6e03c82b3e4f19ecc1ce89684dbb6987147a1e
JuanEstebanFlorezCarmona <jesteban-florez@hotmail.com>
2025-04-22 21:10:06 -0500

    Add files via upload

---
+++ ejercicio-6.py
@@ -0,0 +1,6 @@
+numero = 7
+usuario = int(input("ingresa un numero: "))
+if  usuario < numero:print("Tu numero es menor")
+elif usuario > numero:print("Tu numero es mayor")
+elif usuario == numero: print("Tu numero es igual")
+else: print("Numero no valido")
